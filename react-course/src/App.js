5e909aa146a1688ee2b0dd2648b26ecf5312e9dc
zakumann <samuel742able@gmail.com>
2023-09-21 13:19:05 +0900

    09212023

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
  
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupsPage from "./pages/NewMeetups";
+import MainNavigation from "./components/layout/MainNavigation";
  
 const App = () => {
   return (
+    <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
-        <Route path="/new-meet" element={<NewMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupsPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
+    </div>
   );
 };
  
