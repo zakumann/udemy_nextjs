650caf5d5ba7daca2fe134b367b78b9bbb0f7a2c
zakumann <samuel742able@gmail.com>
2023-10-01 23:15:51 +0900

    10012023 and add nextjs2

---
+++ react-course/src/firebase/app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMsLMGk1qYPkJcuTjxwHXK3mZXb8k7xIA",
+  authDomain: "react-getting-started-434ea.firebaseapp.com",
+  projectId: "react-getting-started-434ea",
+  storageBucket: "react-getting-started-434ea.appspot.com",
+  messagingSenderId: "1029943973367",
+  appId: "1:1029943973367:web:cc75a44e3c17ff57fd0af2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- react-course/src/index.js
+++ react-course/src/index.js
@@ -4,14 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import FavoritesContextProvider from './store/favorites-context';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <FavoritesContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FavoritesContextProvider>
   </React.StrictMode>
 );
 
