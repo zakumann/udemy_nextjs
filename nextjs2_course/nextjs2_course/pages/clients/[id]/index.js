d111a20b7b22853f351130c018f4ab65b4a072e5
zakumann <samuel742able@gmail.com>
2023-10-04 23:36:56 +0900

    10042023

--- nextjs2_course/nextjs2_course/pages/clients/[id]/index.js
+++ nextjs2_course/nextjs2_course/pages/clients/[id]/index.js
@@ -4,9 +4,19 @@ function ClientProjectsPage(){
     const router = useRouter();
 
     console.log(router.query);
+
+    function loadProjectHandler(){
+        // load data...
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: { id: 'samuel', clientprojectid: 'projecta' },
+        });
+    }
+
     return(
         <div>
             <h1>The Projects of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 }
