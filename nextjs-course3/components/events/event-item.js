48caab075d4a4a89b2e7ce5ba2064a00060b58e3
zakumann <samuel742able@gmail.com>
2023-10-11 01:46:32 +0900

    10112023

--- nextjs-course3/components/events/event-item.js
+++ nextjs-course3/components/events/event-item.js
@@ -1,6 +1,36 @@
+import Link from 'next/link';
+
+import classes from './event-item.module.css';
+
 function EventItem(props){
+    const { title, image, date, location, id } = props;
+
+    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+    });
+    const formattedAddress = location.replace(', ', '\n');
+    const exploreLink = `/events/${id}`;
+
     return(
-    <li></li>
+    <li className={classes.item}>
+        <img src={'/' + image} alt={title} />
+        <div className={classes.content}>
+            <div className={classes.summary}>
+                <h2>{title}</h2>
+                <div className={classes.date}>
+                    <time>{humanReadableDate}</time>
+                </div>
+                <div className={classes.address}>
+                    <adderss>{formattedAddress}</adderss>
+                </div>
+            </div>
+            <div className={classes.actions}>
+                <Link href={exploreLink}>Explore Event</Link>
+            </div>
+        </div>
+    </li>
     );
 }
 
