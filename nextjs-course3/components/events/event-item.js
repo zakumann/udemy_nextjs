b26b5c76350f79ff34a2df55b9a10e38e997bcf9
zakumann <samuel742able@gmail.com>
2023-10-17 13:33:46 +0900

    Add search function

--- nextjs-course3/components/events/event-item.js
+++ nextjs-course3/components/events/event-item.js
@@ -4,43 +4,43 @@ import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
 import classes from './event-item.module.css';
 
-function EventItem(props){
-    const { title, image, date, location, id } = props;
+function EventItem(props) {
+  const { title, image, date, location, id } = props;
 
-    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-    });
-    const formattedAddress = location.replace(', ', '\n');
-    const exploreLink = `/events/${id}`;
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const formattedAddress = location.replace(', ', '\n');
+  const exploreLink = `/events/${id}`;
 
-    return(
+  return (
     <li className={classes.item}>
-        <img src={'/' + image} alt={title} />
-        <div className={classes.content}>
-            <div className={classes.summary}>
-                <h2>{title}</h2>
-                <div className={classes.date}>
-                    <DateIcon />
-                    <time>{humanReadableDate}</time>
-                </div>
-                <div className={classes.address}>
-                    <AddressIcon />
-                    <adderss>{formattedAddress}</adderss>
-                </div>
-            </div>
-            <div className={classes.actions}>
-                <Button link={exploreLink}>
-                    <span>Explore Event</span>
-                    <span className={classes.icon}>
-                        <ArrowRightIcon />
-                    </span>
-                </Button>
-            </div>
+      <img src={'/' + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
         </div>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
     </li>
-    );
+  );
 }
 
 export default EventItem;
--- nextjs-course3/pages/events/index.js
+++ nextjs-course3/pages/events/index.js
@@ -1,8 +1,17 @@
+import { Fragment } from 'react';
+
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+
 function AllEventsPage(){
+    const events = getAllEvents();
+
     return(
-        <div>
-            <h1>All Events</h1>
-        </div>
+        <Fragment>
+            <EventsSearch />
+            <EventList items={events} />
+        </Fragment>
     );
 }
 
