f8d965ee5360ead6f8b5b2a269dda5292be4747c
zakumann <samuel742able@gmail.com>
2023-11-05 17:07:20 +0900

    11052023

---
+++ nextjs-course4/pages/[pid].js
@@ -0,0 +1,35 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+import { Fragment } from 'react';
+
+function ProductDetailPage(){
+    const { loadedProduct } = props;
+
+    return (
+    <Fragment>
+        <h1>{loadedProduct.title}</h1>
+        <p>{loadedProduct.description}</p>
+    </Fragment>
+    )
+}
+
+export async function getStaticProps(context){
+    const { params } = context;
+
+    const productId = params.pid;
+
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    const product = data.products.find(product => product.id === productId);
+
+    return {
+        props: {
+            loadedProduct: product
+        }
+    }
+}
+
+export default ProductDetailPage;
--- nextjs-course4/pages/index.js
+++ nextjs-course4/pages/index.js
@@ -1,19 +1,46 @@
-export default function Home(props) {
+import path from 'path';
+import fs from 'fs/promises';
+
+import Link from 'next/link';
+
+function HomePage(props) {
   const { products } = props;
 
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
 }
 
-export async function getStaticProps(){
+export async function getStaticProps(context){
+  console.log('(Re-)Generating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if(!data){
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  }
+
+  if (data.products.length === 0){
+    return { notFound: true };
+  }
+
   return { 
     props: {
-      products : [{id: 'p1', title: 'Product 1'}],
-  },
-};
+      products : data.products
+    },
+    revalidate: 10,
+  };
 }
+
+export default HomePage;
